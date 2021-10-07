2b985351f85d6e4f0fcfc8cd69ac738b3ed4f40f
wolwerr <ricardo@dtmm.com.br>
2021-10-06 23:07:19 -0300

    first commit

---
+++ rick-morty/src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1> Lista de personagens </h1>
+    <span>{{ listPersonagens }}</span>
+    
+    <h1> Lista de Episodios </h1>
+    <span>{{ listEpisodios }}</span>
+
+    <h1> Lista de Locais </h1>
+    <span>{{ listLocais }}</span>
+  </div>
+</template>
+
+<script>
+
+import { mapActions, mapGetters } from 'vuex';
+export default {
+  name: 'Home',
+  mounted(){
+    this.getPersonagens()
+      this.getEpisodios()
+        this.getLocais()
+     
+  },
+  methods:{
+    ...mapActions('personagem', ['getPersonagens']),
+    ...mapActions('episodio', ['getEpisodios']),
+    ...mapActions('local', ['getLocais'])
+  },
+  computed:{
+    ...mapGetters('personagem', ["listPersonagens"]),
+    ...mapGetters('episodio', ["listEpisodios"]),
+    ...mapGetters('local', ["listLocais"])
+  }
+}
+</script>
---
+++ rick-morty/src/store/modules/personagem.js
@@ -0,0 +1,27 @@
+import serviceApi from '../../service/personagem'
+
+export default {
+    namespaced: true,
+    state: {
+        personagens:[]
+    },
+    mutations: {
+        SET_PERSONAGENS(state, payload){
+            state.personagens = payload
+        }
+    },
+    actions: {
+        getPersonagens(context){
+            return serviceApi.getPersonagens().then(res => {
+                context.commit('SET_PERSONAGENS', res.data.results)
+            })
+        }
+    },
+    getters: {
+        listPersonagens(state){
+            return state.personagens
+        }
+    },
+    modules: {
+    }
+}
